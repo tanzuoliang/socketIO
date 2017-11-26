0ff31b15c221b83d2b56e43c4acac96436aa4733
tanzuoliang <ysjwdaypm@163.com>
2017-11-26 16:29:34 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+"use strict"
+let io = require("socket.io")(1234);
+let socketMap = new Map();
+io.on('connection',(socket)=>{
+	console.log(socket.id);
+	console.log("client connected! %j:%j", socket.conn.remoteAddress, socket.conn.remotePort);
+
+	broadMessage(socket.id + " entry into romm");
+	socketMap.set(socket.id,socket);
+	socket.on('disconnect',()=>{
+		console.log("socket disconnect");
+		socketMap.delete(socket.id);
+		broadMessage(socket.id + " remove room");
+	});
+
+	socket.on('message',(msg)=>{
+			console.log("get message " + msg);
+			broadMessage(msg);
+	});
+});
+
+function broadMessage(msg){
+	for(let [_,socket] of socketMap){
+		socket.emit("message",msg);
+		 console.log("broadMessage " + msg + " to client " + socket.id);
+	}
+}
